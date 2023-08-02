6e683bf88dfaa7bb2adf544e952f19a11914dfcc
OmarMWarraich <earthling986@gmail.com>
2023-08-02 10:12:55 +0500

    feat: Create Slices

--- src/features/products/Products.tsx
+++ src/features/products/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getProducts, Product } from "../../app/api";
 import styles from "./Products.module.css";
 
