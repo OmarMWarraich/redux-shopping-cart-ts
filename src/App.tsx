ae19f519206bed2a3659d9bf7a989e94c29d3740
OmarMWarraich <earthling986@gmail.com>
2023-08-02 09:59:03 +0500

    feat: Assets and Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Products } from "./features/products/Products";
+import { CartLink } from "./features/cart/CartLink";
+import { Cart } from "./features/cart/Cart";
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className={styles.app}>
+        <header className={styles.header}>
+          <nav>
+            <Link className={styles.navLink} to="/">
+              Home
+            </Link>
+            <Link className={styles.navLink} to="/products">
+              Products
+            </Link>
+            <CartLink />
+          </nav>
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
+function Home() {
+  return (
+    <main className="page">
+      <h1>Welcome to the Store</h1>
+      <figure>
+        <img src="/store.jpg" alt="A large old storefront" width="800" />
+        <figcaption>Gary Houston, CC0, via Wikimedia Commons</figcaption>
+      </figure>
+    </main>
+  );
+}
